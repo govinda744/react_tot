0e06fc32a53e500fa8a15c92dfc5f5a15a2ae6ca
Govinda Bhandari <govinda.bhandari744@gmail.com>
2020-02-14 13:02:50 +0545

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,108 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Switch, Route, NavLink } from 'react-router-dom';
+import Home from './Components/Home';
+import Completed from './Components/Completed';
+import Remaining from './Components/Remaining';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      searchText: '',
+      todos: []
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/todos')
+    .then((response) => {return response.json()})
+    .then((data) => this.setState({
+      todos : data
+    }));
+  }
+
+  getTodoList() {
+    fetch()
+  }
+
+  filterItems = (items = [], serachText = '') => items.filter(item => item.title.includes(serachText))
+
+  completedTodos() {
+    return this.filterItems(this.state.todos, this.state.searchText).filter((value) => value.completed);
+  }
+
+  remainingTodos() {
+    return this.filterItems(this.state.todos, this.state.searchText).filter((value) => !value.completed);
+  }
+
+
+  addTodo = todoTitle => {
+    if (todoTitle !== '') {
+      this.setState({
+        todos: [...this.state.todos, {
+          id: this.state.todos.length + 1,
+          title: todoTitle,
+          completed: false
+        }]
+      });
+    }
+  }
+
+  changeTodoStatus = id => {
+    this.setState(
+      {
+        todos: this.state.todos.map(todo => {
+          if (todo.id === id) {
+            todo.completed = !todo.completed
+          }
+          return todo;
+        })
+      }
+    );
+  }
+
+  searchItem = searchText => {
+    this.setState({ searchText });
+  }
+
+  render() {
+    return (
+      <Router>
+        <div className='container'>
+          <header>
+            <ul>
+              <li onClick={() => this.searchItem('')}>
+                <NavLink activeClassName='' to='/'><img src='https://pngimage.net/wp-content/uploads/2018/06/todo-png-5.png' alt='icon'></img></NavLink>
+              </li>
+              <li onClick={() => this.searchItem('')}>
+                <NavLink exact={true} activeClassName='is-active' to='/'>Home</NavLink>
+              </li>
+              <li onClick={() => this.searchItem('')}>
+                <NavLink activeClassName='is-active' to='/completed'>Completed</NavLink>
+              </li>
+              <li onClick={() => this.searchItem('')}>
+                <NavLink activeClassName='is-active' to='/remaining'>Remainig</NavLink>
+              </li>
+            </ul>
+          </header>
+
+          <Switch>
+            <Route exact path='/'>
+              <Home todos={this.filterItems(this.state.todos, this.state.searchText)} addTodo={this.addTodo} changeTodoStatus={this.changeTodoStatus} searchItem={this.searchItem} />
+            </Route>
+            <Route exact path='/completed'>
+              <Completed todos={this.completedTodos()} addTodo={this.addTodo} changeTodoStatus={this.changeTodoStatus} searchItem={this.searchItem} />
+            </Route>
+            <Route exact path='/remaining'>
+              <Remaining todos={this.remainingTodos()} addTodo={this.addTodo} changeTodoStatus={this.changeTodoStatus} searchItem={this.searchItem} />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
---
+++ src/Components/Remaining.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+
+export class Remaining extends Component {
+  render() {
+    const { todos, changeTodoStatus, addTodo, searchItem } = this.props;
+    return (
+      <div>
+        <div className='fieldInput'>
+          <input type='text' placeholder='search' defaultValue='' onChange={(e) => searchItem(e.target.value)} />
+        </div>
+        <div className='fieldInput'>
+          <input type='text' placeholder='Add todo' defaultValue='' onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              addTodo(e.target.value);
+              e.target.value = '';
+            }
+          }} />
+        </div>
+        <hr />
+        <div>
+          <ul>
+            {todos.length === 0 ? 
+            <li key='0' style={{lineHeight: '100px'}}>No remaining todo</li> :
+            todos.map((value, index) => {
+              return (<li key={index} onClick={() => changeTodoStatus(value.id)}>{index + 1 + '. '+value.title}</li>)
+            })}
+          </ul>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Remaining
